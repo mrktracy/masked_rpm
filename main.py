19abcfbc4f9704ee82280018da64dba5e59369b5
Mark <mtracy03@gmail.com>
2023-07-28 16:14:29 -0400

    main.py outlined and main_ae.py filled in

--- main.py
+++ main.py
@@ -1,2 +1,78 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision.models import resnet18
+
+# 1. Data Loader
+class RPMProblemDataset(Dataset):
+    def __init__(self, data_path):
+        # Load .npz files here
+        pass
+
+    def __getitem__(self, idx):
+        # Generate "masked sentences" and labels
+        pass
+
+    def __len__(self):
+        # Return the total number of samples
+        pass
+
+
+# 2. CNN Autoencoder
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        # Use a standard CNN such as resnet and make an autoencoder
+        pass
+
+    def forward(self, x):
+        pass
+
+
+# 3. Transformer Model
+class TransformerModel(nn.Module):
+    def __init__(self):
+        super(TransformerModel, self).__init__()
+        # Define your transformer layers here
+        pass
+
+    def forward(self, x, mask):
+        # Pass the input through transformer layers
+        pass
+
+
+# 4. Evaluation Module
+def evaluate_model(model, dataset):
+    # Evaluation logic here
+    pass
+
+
+def main():
+    # Define Hyperparameters
+    EPOCHS = 10
+    BATCH_SIZE = 32
+    LEARNING_RATE = 0.001
+
+    # Initialize data loader, models, optimizer, and loss function
+    dataset = RPMProblemDataset('data_path')
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    autoencoder = Autoencoder()
+    transformer_model = TransformerModel()
+
+    optimizer = torch.optim.Adam(list(autoencoder.parameters()) + list(transformer_model.parameters()),
+                                 lr=LEARNING_RATE)
+    criterion = nn.MSELoss()
+
+    # Training loop
+    for epoch in range(EPOCHS):
+        for i, (inputs, targets) in enumerate(dataloader):
+            # Write your training code here
+            pass
+
+    # Evaluate the model
+    evaluate_model(transformer_model, dataset)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_ae.py
@@ -0,0 +1,165 @@
+# 1. Dataset
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
+import numpy as np
+import random
+
+def gather_files(root_dir):
+    all_files = []
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith('.npz'):
+                all_files.append(os.path.join(dirpath, filename))
+    random.shuffle(all_files)
+    return all_files
+
+class RPMPanels(Dataset):
+    def __init__(self, files):
+        self.files = files
+
+    def __getitem__(self, idx):
+        fileidx = idx // 16
+        panelidx = idx % 16
+        filename = self.files[fileidx]
+        data = np.load(filename)
+        image = data['image']
+        panel = torch.from_numpy(image[panelidx,:,:]).float()
+        label = panel.clone()
+
+        return (panel.unsqueeze(0), label.unsqueeze(0))
+
+    def __len__(self):
+        length = len(self.files)*16
+        return length
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+class ResNetAutoencoder(nn.Module):
+    def __init__(self):
+        super(ResNetAutoencoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+            ResidualBlock(1, 64),
+            ResidualBlock(64, 128, 2),
+            ResidualBlock(128, 256, 2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 1, kernel_size=2, stride=2),
+            nn.Sigmoid(),  # to ensure the output is in [0, 1] as image pixel intensities
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # add this line
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    def get_embedding(self, x):
+        x = self.encoder(x)
+        x = self.avgpool(x)
+        return x.view(x.size(0), -1)  # reshaping to (batch_size, 256)
+
+def evaluate_model(model, dataloader, device):
+    model.eval()
+    with torch.no_grad():
+        criterion = nn.MSELoss()
+        total_loss = 0
+        for batch in dataloader:
+            # assuming that the data loader returns images and labels, but we don't need labels here
+            images, _ = batch
+            # move images to the device, reshape them and ensure channel dimension is present
+            images = images.to(device)
+            # forward pass
+            outputs = model(images)
+            loss = criterion(outputs, images)
+            total_loss += loss.item()
+
+    return total_loss/len(dataloader.dataset)
+
+def main():
+    # Define Hyperparameters
+    EPOCHS = 1
+    BATCH_SIZE = 32
+    LEARNING_RATE = 0.001
+
+    # Initialize device, data loader, model, optimizer, and loss function
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    root_dir = '/path/to/dataset'
+    all_files = gather_files(root_dir)
+    num_files = len(all_files)
+    train_proportion = 0.7
+    val_proportion = 0.15
+    # test proportion is 1 - train_proportion - val_proportion
+    train_files = all_files[:int(num_files*train_proportion)]
+    val_files = all_files[int(num_files * train_proportion):int(num_files * train_proportion+val_proportion)]
+    test_files = all_files[int(num_files * train_proportion+val_proportion):]
+
+    train_dataset = RPMPanels(train_files)
+    val_dataset = RPMPanels(val_files)
+    test_dataset = RPMPanels(test_files)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    autoencoder = ResNetAutoencoder().to(device)
+
+    optimizer = torch.optim.Adam(list(autoencoder.parameters()),
+                                 lr=LEARNING_RATE)
+    criterion = nn.MSELoss()
+
+    # Training loop
+    for epoch in range(EPOCHS):
+        for batch in train_dataloader:
+            # assuming that the data loader returns images and labels, but we don't need labels here
+            images, _ = batch
+
+            # move images to the device, reshape them and ensure channel dimension is present
+            images = images.to(device).view(-1, 1, 160, 160)
+
+            # forward pass
+            outputs = autoencoder(images)
+            loss = criterion(outputs, images)
+
+            # backward pass and optimization
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, EPOCHS, loss.item()))
+
+    # Evaluate the model
+    avg_val_loss = evaluate_model(autoencoder, val_dataloader, device)
+    print(f"Average validation loss: {avg_val_loss}")
+
+if __name__ == "__main__":
+    main()
