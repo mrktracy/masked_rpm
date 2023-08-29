3d25131c076ef530f04ef2216b381f2685f1a569
Mark <mtracy03@gmail.com>
2023-08-29 16:34:35 -0400

    Set up mini run on RAVEN data to test v4-itr0 model

--- main_tr.py
+++ main_tr.py
@@ -81,6 +81,9 @@ def main():
     val_files = all_files[int(num_files * train_proportion):int(num_files * (train_proportion + val_proportion))]
     # test_files = all_files[int(num_files * (train_proportion + val_proportion)):]
 
+    train_files = train_files[0:256]
+    val_files = train_files[0:256]
+
     train_dataset = RPMSentencesNew(train_files, autoencoder, device=device)
     val_dataset = RPMSentencesNew(val_files, autoencoder, device=device)
 
