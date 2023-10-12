2e81277958b917a08021b94e4e039dc909bd8db1
Mark <mtracy03@gmail.com>
2023-10-11 21:22:37 -0400

    Attempt full run on v7-itr1

--- datasets.py
+++ datasets.py
@@ -23,8 +23,9 @@ class RPMSentencesViT(Dataset):
     def __getitem__(self, idx):
         filename = self.files[idx]
         data = np.load(filename)
-        image = data['image'].reshape(16, 160, 160).unsqueeze(1)
+        image = data['image'].reshape(16, 1, 160, 160)
         image = torch.from_numpy(image).float() / 255 # convert context panels to tensor
+        image = image.unsqueeze(1) # add channel dimension
 
         # Preprocessing for ViT
         inputs = self.feature_extractor(images=image, return_tensors="pt", \
