dcbd46e63969e83604423526ce17e46a62a894d5
Mark <mtracy03@gmail.com>
2023-10-25 21:21:16 -0400

    Attempt run v8-itr0 with masked sentences

--- evaluate_masked.py
+++ evaluate_masked.py
@@ -9,22 +9,13 @@ def evaluate_model_masked(model, dataloader, device, max_batches = None):
         num_samples = 0
         for idx, (inputs, candidates, targets) in enumerate(dataloader):
 
-            # batch_size = inputs.shape[0]
-
             # move images to the device
             inputs = inputs.to(device) # shape (B,9,model_dim)
             candidates = candidates.to(device) # shape (B, 8, embed_dim)
             targets = targets.to(device) # shape (B,)
 
-
             # forward pass
-            outputs = model(inputs) # (batch_size,embed_dim)
-
-            print(f"{inputs.shape=}")
-            print(f"{targets.shape=}")
-            print(f"{candidates.shape=}")
-            print(f"{outputs.shape=}")
-
+            outputs = model(inputs).unsqueeze(1) # (batch_size,1,embed_dim)
             guesses = torch.argmin(torch.sum((candidates - outputs)**2, dim=-1), dim = -1)
             num_correct += torch.eq(guesses, targets).sum().item()
             num_samples += inputs.size(0)
