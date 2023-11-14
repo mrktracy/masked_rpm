e51de39a17d1bed247030072ab722de6eab4fd55
Mark <mtracy03@gmail.com>
2023-11-14 16:50:40 -0500

    v10-itr11, alpha = 1/160**2, base lr = 0.0001

--- main_masked.py
+++ main_masked.py
@@ -14,7 +14,7 @@ from models import TransformerModelv9, TransformerModelv8, TransformerModelv10
 import os
 import logging
 
-logfile = "../tr_results/v10-itr10/runlog.txt"
+logfile = "../tr_results/v10-itr11/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -101,9 +101,9 @@ def main_BERT():
     LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 50
     EPOCHS_PER_SAVE = 1
-    VERSION = "v10-itr10"
+    VERSION = "v10-itr11"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
-    ALPHA = 0.01 # scaling regularizer
+    ALPHA = 1/160**2 # scaling regularizer
     DELTA = 1e-8 # for log stability
 
     ''' Instantiate data loaders, optimizer, criterion '''
