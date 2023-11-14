ff07debb9594a9e32c4d0626b6d32e64dd13119f
Mark <mtracy03@gmail.com>
2023-11-14 16:11:49 -0500

    v10-itr10, alpha = 0.5/160^2, base lr = 0.001, remove dropout

--- main_masked.py
+++ main_masked.py
@@ -14,7 +14,7 @@ from models import TransformerModelv9, TransformerModelv8, TransformerModelv10
 import os
 import logging
 
-logfile = "../tr_results/v10-itr9/runlog.txt"
+logfile = "../tr_results/v10-itr10/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -310,9 +310,9 @@ def main_BERT():
     LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 50
     EPOCHS_PER_SAVE = 1
-    VERSION = "v10-itr9"
+    VERSION = "v10-itr10"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
-    ALPHA = 0.1/160**2 # scaling regularizer
+    ALPHA = 0.5/160**2 # scaling regularizer
     DELTA = 1e-8 # for log stability
 
     ''' Instantiate data loaders, optimizer, criterion '''
