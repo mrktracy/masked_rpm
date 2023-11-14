50aacbd5f8eaa3be3b2971ed20cd66b5b1bda9e6
Mark <mtracy03@gmail.com>
2023-11-14 11:19:43 -0500

    Run v10-itr2, lr = 0.0001, write gradients, add encoding and decoding end-to-end, BERT-style masking, rotated grid augmentation, depth=10; add entropy regularization term to objective

--- main_masked.py
+++ main_masked.py
@@ -344,7 +344,8 @@ def main_BERT():
             mask_tensors = mask_tensors.to(device)
 
             outputs = transformer_model(inputs, mask_tensors) # (B,1,160,160)
-            loss = criterion(outputs,targets) + ALPHA*torch.mean(torch.sum(output*torch.log(output + DELTA), dim=[1,2,3]))
+            regularizer = ALPHA*torch.mean(torch.sum(outputs*torch.log(outputs + DELTA), dim=[1,2,3]))
+            loss = criterion(outputs,targets) + regularizer
 
             tot_loss += loss.item() # update running averages
             count += 1
