44343f72e6d3200c6886ec65ebc8b313fa271795
Mark <mtracy03@gmail.com>
2024-02-20 12:03:39 -0500

    v13-itr0. Multiplicative loss. Guess error on single panel in embedding space

--- main_masked.py
+++ main_masked.py
@@ -159,7 +159,7 @@ def main_BERT():
                 # print(f"Output all zeros: {torch.equal(outputs, torch.zeros_like(outputs))}")
 
             if (idx+1) % batches_per_log == 0:
-                val_loss = evaluate_model_masked_BERT_v13(transformer_model, val_dataloader, device, max_batches=150)
+                val_loss = evaluate_model_masked_BERT_v13(original_model, val_dataloader, device, max_batches=150)
                 output = f"Epoch {epoch+1} - {idx+1}/{train_length}. loss: {tot_loss/count:.4f}. lr: {scheduler.get_last_lr()[0]:.6f}. val: {val_loss:.2f}\n"
                 # output = f"Epoch {epoch + 1} - {idx + 1}/{train_length}. loss: {tot_loss / count:.4f}."
                 print(output)
