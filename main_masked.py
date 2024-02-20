903b29035f6b774ce92cb3c893047da06731f900
Mark <mtracy03@gmail.com>
2024-02-20 12:22:10 -0500

    v13-itr1. Additive loss. Guess error on single panel in embedding space

--- main_masked.py
+++ main_masked.py
@@ -14,7 +14,7 @@ from models import TransformerModelv9, TransformerModelv8, TransformerModelv10, 
 import os
 import logging
 
-logfile = "../tr_results/v13-itr0/runlog.txt"
+logfile = "../tr_results/v13-itr1/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -95,7 +95,7 @@ def main_BERT():
     LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 500
     # EPOCHS_PER_SAVE = 1
-    VERSION = "v13-itr0"
+    VERSION = "v13-itr1"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
     # ALPHA_1 = 1/(9*160**2) # scaling regularizer
     ALPHA_2 = 0.5 # for relative importance of guess vs. autoencoder accuracy
