89ce2e48ffc4466ab502b12bde9d50a6c5a7efd1
Mark <mtracy03@gmail.com>
2024-02-21 22:17:25 -0500

    v14-itr2. Full run. Cross-Entropy loss between one-hot encoding. ALPHA_2 = 1, depth = 10

--- main_masked.py
+++ main_masked.py
@@ -92,7 +92,7 @@ def main_BERT():
     BATCH_SIZE = 32
     LEARNING_RATE = 0.001
     # MOMENTUM = 0.90
-    LOGS_PER_EPOCH = 1
+    LOGS_PER_EPOCH = 100
     BATCHES_PER_PRINT = 50
     EPOCHS_PER_SAVE = 5
     VERSION = "v14-itr2"
