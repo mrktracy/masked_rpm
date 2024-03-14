c42a504a6dfd8ae029cc86e383b8c8c220a57cb0
Mark <mtracy03@gmail.com>
2024-03-14 17:32:00 -0400

    v15-itr3, using symbols and TCN. ALPHA_2 = 0.75, recreation error on decoder factored in. Limited run. Cross-Entropy loss between one-hot encoding. depth = 10, Gamma = 1.

--- main_masked.py
+++ main_masked.py
@@ -95,10 +95,10 @@ def main_BERT():
     LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 30
     EPOCHS_PER_SAVE = 500
-    VERSION = "v15-itr2"
+    VERSION = "v15-itr3"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
     # ALPHA_1 = 1/(9*160**2) # scaling regularizer
-    ALPHA_2 = 1 # for relative importance of guess vs. autoencoder accuracy
+    ALPHA_2 = 0.75 # for relative importance of guess vs. autoencoder accuracy
     # ALPHA_3 = 10000 # for scaling loss when multiplying errors
     # DELTA = 1e-8 # for log stability
 
