0e71123bf97f806a0871d1606038533327033093
Mark <mtracy03@gmail.com>
2024-03-14 17:56:36 -0400

    v15-itr3, using symbols and TCN. ALPHA_2 = 1, recreation error on decoder factored in. Limited run. Cross-Entropy loss between one-hot encoding. depth = 10, Gamma = 1.

--- models.py
+++ models.py
@@ -98,7 +98,7 @@ class TransformerModelv15(nn.Module): # takes in images, embeds, performs self-a
         ims_reshaped = ims.view(-1, 1, 160, 160)  # x is (B, 9, 1, 160, 160)
         x_reshaped = self.perception.forward(ims_reshaped) # x_reshaped is (B*9, embed_dim)
         x = x_reshaped.view(batch_size, 9, -1) # x is (B, 9, embed_dim)
-        x = self.tcn(x, num_features=768)
+        x = self.tcn(x)
 
         cands_reshaped = cands.view(-1, 1, 160, 160)  # cands is (B, 8, 1, 160, 160)
         cands_reshaped = self.perception.forward(cands_reshaped)  # cands_reshaped is (B*8, embed_dim)
