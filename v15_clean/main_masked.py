be50b32e2d8925786662fe42f9dcde187cdb5499
Mark <mtracy03@gmail.com>
2024-03-20 13:29:44 -0400

    v15-itr9, using wider symbols and TCN. ALPHA_2 = 0.5, lr = 0.005, recreation error on decoder included. Full run. MSE loss between abstract encoding. depth = 10, Gamma = 0.95

--- v15_clean/main_masked.py
+++ v15_clean/main_masked.py
@@ -34,7 +34,7 @@ def main_BERT():
     # Initialize device, model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     num_gpus = torch.cuda.device_count()
-    print(num_gpus)
+    # print(num_gpus)
 
     transformer_model = TransformerModelv15(depth=10, num_heads=64, cat=True).to(device)
 
