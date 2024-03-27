6a4f4231876021e239c9203a261821ee3af807c3
Mark <mtracy03@gmail.com>
2024-03-27 09:26:53 -0400

    v15-itr17, using 1x wider symbols and TCN. ALPHA = 0.75, lr = 0.0001, Full run. MSE loss between abstract embedding. depth = 15, heads = 64, Gamma = 0.99. Correct skip connection to use x_v; separate LayerNorm for x_v.

--- v15_clean/main_masked.py
+++ v15_clean/main_masked.py
@@ -36,7 +36,7 @@ def main_BERT():
     num_gpus = torch.cuda.device_count()
     # print(num_gpus)
 
-    transformer_model = TransformerModelv15(symbol_factor = 2, depth=10, num_heads=64, cat=True).to(device)
+    transformer_model = TransformerModelv15(symbol_factor = 1, depth=15, num_heads=64, cat=True).to(device)
 
     # initialize weights
     transformer_model.apply(initialize_weights_he)
