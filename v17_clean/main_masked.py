5c3241b71a89e8d08cf44923a932accb246b688c
Mark <mtracy03@gmail.com>
2024-04-02 15:05:17 -0400

    v17-itr0, 15 epochs of training, use backbone perception, embed_dim = 768, using 1x wider symbols and TCN, abstractor and transformer blocks in parallel, concatenated, ALPHA = 0.75, lr = 0.0001, Full run, constant only. CE loss between distribution over options. depth = 5, heads = 64, Gamma = 0.99. Response elimination via scoring

--- v17_clean/main_masked.py
+++ v17_clean/main_masked.py
@@ -71,11 +71,11 @@ def main_BERT():
                                             device=device)
 
     ''' Define Hyperparameters '''
-    EPOCHS = 15
+    EPOCHS = 300
     BATCH_SIZE = 32
     LEARNING_RATE = 0.0001
     # MOMENTUM = 0.90
-    LOGS_PER_EPOCH = 10
+    LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 15
     VERSION = "v17-itr0"
