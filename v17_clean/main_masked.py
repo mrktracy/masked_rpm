68353b5332fdb14d691b04b3dd3c13ef38cd219b
Mark <mtracy03@gmail.com>
2024-04-03 22:26:07 -0400

    v17-itr1_full, 15 epochs of training, use backbone perception, embed_dim = 192, using 2x wider symbols and TCN, abstractor and transformer blocks in parallel, concatenated, ALPHA = 0.75, lr = 0.0001, Full run, all rules. CE loss between distribution over options. main blocks depth = 3, backbone depth = 2, heads = 32, Gamma = 0.99. Response elimination via scoring

--- v17_clean/main_masked.py
+++ v17_clean/main_masked.py
@@ -36,8 +36,8 @@ def main_BERT():
     num_gpus = torch.cuda.device_count()
     # print(num_gpus)
 
-    transformer_model = TransformerModelv17(embed_dim = 192, symbol_factor=2, depth=5, num_heads=64, cat_pos=True, \
-                                            cat_output=True, use_backbone=True, backbone_depth=3).to(device)
+    transformer_model = TransformerModelv17(embed_dim = 192, symbol_factor=2, depth=3, num_heads=64, cat_pos=True, \
+                                            cat_output=True, use_backbone=True, backbone_depth=2).to(device)
 
     # initialize weights
     transformer_model.apply(initialize_weights_he)
