31f2c1594785ba94c7c81cd336c893efcf2ce590
Mark <mtracy03@gmail.com>
2024-04-03 22:24:31 -0400

    v17-itr1_full, 15 epochs of training, use backbone perception, embed_dim = 192, using 2x wider symbols and TCN, abstractor and transformer blocks in parallel, concatenated, ALPHA = 0.75, lr = 0.0001, Full run, all rules. CE loss between distribution over options. main blocks depth = 5, backbone depth = 4, heads = 32, Gamma = 0.99. Response elimination via scoring

--- v17_clean/main_masked.py
+++ v17_clean/main_masked.py
@@ -12,7 +12,7 @@ from models import TransformerModelv17
 import os
 import logging
 
-logfile = "../../tr_results/v17-itr0_full_100/runlog.txt"
+logfile = "../../tr_results/v17-itr1_full/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -36,7 +36,7 @@ def main_BERT():
     num_gpus = torch.cuda.device_count()
     # print(num_gpus)
 
-    transformer_model = TransformerModelv17(embed_dim = 192, symbol_factor=3, depth=5, num_heads=32, cat_pos=True, \
+    transformer_model = TransformerModelv17(embed_dim = 192, symbol_factor=2, depth=5, num_heads=64, cat_pos=True, \
                                             cat_output=True, use_backbone=True, backbone_depth=4).to(device)
 
     # initialize weights
@@ -61,8 +61,8 @@ def main_BERT():
     # root_dir = '../../i_raven_data_cnst/'
     root_dir = '../../i_raven_data_full/'
     train_files, val_files, test_files = gather_files_pgm(root_dir)
-    train_files = train_files[:100]
-    val_files = val_files[:100]
+    # train_files = train_files[:100]
+    # val_files = val_files[:100]
 
     ''' Transformer model v9 '''
     train_dataset = RPMFullSentencesRaw_v2(train_files, \
@@ -78,7 +78,7 @@ def main_BERT():
     LOGS_PER_EPOCH = 1
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 50
-    VERSION = "v17-itr0_full_100"
+    VERSION = "v17-itr1_full"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
     ALPHA = 0.75 # for relative importance of guess vs. autoencoder accuracy
 
