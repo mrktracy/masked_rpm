ba0f58b04e6a278c0c7fa1a861a04cd5b5484651
Mark <mtracy03@gmail.com>
2024-04-09 19:24:05 -0400

    v18-itr0_full, 50 epochs of training, use backbone perception, embed_dim = 768, using 2x wider symbols and TCN, transformer blocks in parallel with two sets of abstractor blocks in series, concatenated, ALPHA = 0.5, lr = 0.00005, Full run, all rules. CE loss between distribution over options. main blocks depth = 8, backbone depth = 4, heads = 64, Gamma = 0.99. Response elimination via scoring, aggregate via mean before scoring

--- v17_clean/main_masked.py
+++ v17_clean/main_masked.py
@@ -12,7 +12,7 @@ from models import TransformerModelv17, TransformerModelv18, TransformerModelv19
 import os
 import logging
 
-logfile = "../../tr_results/v19-itr0_full/runlog.txt"
+logfile = "../../tr_results/v18-itr0_full/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -44,29 +44,29 @@ def main_BERT():
     #                                         cat_output=True,
     #                                         use_backbone=True,
     #                                         backbone_depth=4).to(device)
-    # transformer_model = TransformerModelv18(embed_dim=768,
-    #                                         symbol_factor=2,
-    #                                         trans_depth=8,
-    #                                         abs_1_depth=4,
-    #                                         abs_2_depth=4,
-    #                                         trans_num_heads=64,
-    #                                         abs_1_num_heads=64,
-    #                                         abs_2_num_heads=64,
-    #                                         cat_pos=True,
-    #                                         cat_output=True,
-    #                                         use_backbone=True,
-    #                                         bb_depth=4,
-    #                                         bb_num_heads=32).to(device)
-    transformer_model = TransformerModelv19(embed_dim=768,
+    transformer_model = TransformerModelv18(embed_dim=768,
                                             symbol_factor=2,
-                                            trans_depth=5,
-                                            abs_1_depth=5,
+                                            trans_depth=8,
+                                            abs_1_depth=4,
+                                            abs_2_depth=4,
                                             trans_num_heads=64,
                                             abs_1_num_heads=64,
+                                            abs_2_num_heads=64,
                                             cat_pos=True,
+                                            cat_output=True,
                                             use_backbone=True,
                                             bb_depth=4,
                                             bb_num_heads=32).to(device)
+    # transformer_model = TransformerModelv19(embed_dim=768,
+    #                                         symbol_factor=2,
+    #                                         trans_depth=5,
+    #                                         abs_1_depth=5,
+    #                                         trans_num_heads=64,
+    #                                         abs_1_num_heads=64,
+    #                                         cat_pos=True,
+    #                                         use_backbone=True,
+    #                                         bb_depth=4,
+    #                                         bb_num_heads=32).to(device)
     # initialize weights
     transformer_model.apply(initialize_weights_he)
 
@@ -106,7 +106,7 @@ def main_BERT():
     LOGS_PER_EPOCH = 10
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 10
-    VERSION = "v19-itr0_full"
+    VERSION = "v18-itr0_full"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
     ALPHA = 0.75 # for relative importance of guess vs. autoencoder accuracy
 
