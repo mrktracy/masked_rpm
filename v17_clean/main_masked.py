96eac60353af51f194003ac9ffc203fe0fa562f4
Mark <mtracy03@gmail.com>
2024-04-10 16:25:00 -0400

    v19-itr2_full, 50 epochs of training, use backbone perception, embed_dim = 768, using 1x wider symbols and TCN, ternary augmentation, transformer blocks in parallel with one set of abstractor blocks, concatenated, ALPHA = 0.75, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 8, abstractor 1 depth = 8, abstractor 2 depth = NA, heads = 64, backbone heads = 32, Gamma = 0.99. Response elimination via scoring, aggregate via mean before scoring

--- v17_clean/main_masked.py
+++ v17_clean/main_masked.py
@@ -12,7 +12,7 @@ from models import TransformerModelv17, TransformerModelv18, TransformerModelv19
 import os
 import logging
 
-logfile = "../../tr_results/v19-itr1_full/runlog.txt"
+logfile = "../../tr_results/v19-itr2_full/runlog.txt"
 
 os.makedirs(os.path.dirname(logfile), exist_ok=True)
 # logging.basicConfig(filename=logfile,level=logging.INFO, filemode='w')
@@ -105,7 +105,7 @@ def main_BERT():
     LOGS_PER_EPOCH = 5
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 10
-    VERSION = "v19-itr1_full"
+    VERSION = "v19-itr2_full"
     VERSION_SUBFOLDER = "" # e.g. "MNIST/" or ""
     ALPHA = 0.75 # for relative importance of guess vs. autoencoder accuracy
 
