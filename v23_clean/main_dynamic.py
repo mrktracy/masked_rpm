ee9a3c634376bc1a230203408a0f36c104d37093
mtracy03 <mtracy03@gmail.com>
2024-06-19 16:51:17 -0400

    v23-itr0_full, 20 epochs of training, use backbone perception with no dropout (p=0), deconvolution decoder with no dropout (p=0), embed_dim = 256, using 1x wider symbols and TCN, ternary augmentation on rows and columns, transformer blocks in parallel with two sets of abstractor blocks, concatenated, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 2, abstractor 1 depth = 2, abstractor 2 depth = 2, backbone depth = 1, heads = 4, backbone heads = 2, Gamma = 0.95. Response elimination via scoring, aggregate via mean before scoring, mlp_drop = 0.5, proj_drop = 0.5, attn_drop = 0.5, weight_decay = 1e-4, mlp_ratio = 4, no L1 regularization (L1 = 0), data augmentation (rotations), random seed = 42, use I-RAVEN data set, BETA = 2, second model not autoregressive, HISTORY_SIZE = 12, position-only Q and K for transformer on all blocks, RNN  for second model, BATCH_SIZE = 32, 100 features in parallel

--- v23_clean/main_dynamic.py
+++ v23_clean/main_dynamic.py
@@ -12,7 +12,7 @@ from models import TransformerModelv23, DynamicWeighting, DynamicWeightingRNN
 import os
 import logging
 
-version = "v23-itr1_full"
+version = "v23-itr0_full"
 
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
@@ -35,7 +35,7 @@ def main_BERT(VERSION, RESULTS_FOLDER):
 
     MLP_DW = False
     HISTORY_SIZE = 12
-    AUTO_REG = True
+    AUTO_REG = False
 
     if AUTO_REG:
         max_history_length = HISTORY_SIZE*4
@@ -111,7 +111,7 @@ def main_BERT(VERSION, RESULTS_FOLDER):
     LEARNING_RATE = 0.00005
     # MOMENTUM = 0.90
     LOGS_PER_EPOCH = 5
-    BATCHES_PER_PRINT = 20
+    BATCHES_PER_PRINT = 60
     EPOCHS_PER_SAVE = 5
     VERSION_SUBFOLDER = ""  # e.g. "MNIST/" or ""
     # ALPHA = 0.5 # for relative importance of guess vs. autoencoder accuracy
