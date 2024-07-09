e9e8cb5e135575e58afea7862098080857c640b3
mtracy03 <mtracy03@gmail.com>
2024-07-09 13:38:57 -0400

    v22-itr40_full;  20 epochs of training, use backbone perception with no dropout (p=0), deconvolution decoder with no dropout (p=0), embed_dim = 512, using 1x wider symbols and TCN, ternary MLP on rows and columns with dropout (p = 0.5), transformer blocks in parallel with two sets of abstractor blocks, concatenated, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 2, abstractor one depth = 2, abstractor two depth = 2, heads = 4, backbone depth = 1, backbone heads = 2, Gamma = 0.99. Response elimination via scoring, aggregate via mean before scoring, mlp_drop = 0.5, proj_drop = 0.5, attn_drop = 0.5, weight_decay = 1e-4, mlp_ratio = 4, no L1 regularization (L1 = 0), data augmentation (rotations), random seed = 42, use I-RAVEN data set, BETA = 2, second model not autoregressive, HISTORY_SIZE = 12, MLP for second model with no dropout (p=0), relative shares for second model, BATCH_SIZE = 32, restrict_qk = False

--- v22_clean/main.py
+++ v22_clean/main.py
@@ -12,7 +12,7 @@ from models import TransformerModelv22, DynamicWeighting, DynamicWeightingRNN
 import os
 import logging
 
-version = "v22-itr39_full"
+version = "v22-itr40_full"
 
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
@@ -64,12 +64,12 @@ def main_BERT(VERSION, RESULTS_FOLDER):
                                             bb_depth=1,
                                             bb_num_heads=4,
                                             ternary_num=3, # 1 - C, 2 - Hadamard, 3 - MLP
-                                            mlp_drop=0.3,
-                                            proj_drop=0.3,
-                                            attn_drop=0.3,
-                                            drop_path_max=0.3,
+                                            mlp_drop=0.5,
+                                            proj_drop=0.5,
+                                            attn_drop=0.5,
+                                            drop_path_max=0.5,
                                             per_mlp_drop=0,
-                                            ternary_drop=0.3,
+                                            ternary_drop=0.5,
                                             ternary_mlp_ratio=3,
                                             restrict_qk=False).to(device)
     if MLP_DW:
@@ -155,8 +155,8 @@ def main_BERT(VERSION, RESULTS_FOLDER):
                                    lr=LEARNING_RATE,
                                    weight_decay=1e-4)
 
-    scheduler_1 = ExponentialLR(optimizer_1, gamma=0.95)
-    scheduler_2 = ExponentialLR(optimizer_2, gamma=0.95)
+    scheduler_1 = ExponentialLR(optimizer_1, gamma=0.99)
+    scheduler_2 = ExponentialLR(optimizer_2, gamma=0.99)
 
     criterion_1 = nn.CrossEntropyLoss()
     criterion_2 = nn.MSELoss()
