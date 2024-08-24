5552bf3abef5a775ba670ef8a3d22fb3af2a5ae4
mtracy03 <mtracy03@gmail.com>
2024-08-24 09:24:25 -0400

    v24-itr22_full;  20 epochs of training, use backbone perception with no MLP dropout (p=0), deconvolution decoder with no dropout (p=0), embed_dim = 512, using 1x wider symbols and TCN, ternary MLP on rows and columns with dropout (p = 0.3), transformer blocks in parallel with two sets of abstractor blocks, concatenated, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 2, abstractor one depth = 2, abstractor two depth = 2, heads = 4, backbone depth = 1, backbone heads = 4, Gamma = 0.95. Response elimination via scoring, aggregate via mean before scoring, mlp_drop = 0.5, proj_drop = 0.5, attn_drop = 0.5, weight_decay = 1e-4, mlp_ratio = 4, no L1 regularization (L1 = 0), no data augmentation (rotations), random seed = 42, use I-RAVEN dataset, no dynamic loss balancing, BATCH_SIZE = 32, restrict_qk = False, backbone transformer attn_drop = 0.3, backbone transformer proj_drop = 0.3, backbone max proj_drop = 0.5, add meta-reasoning module with feedback to perception, two-layer combiner with ReLU, transformer in meta-reasoning, feedback_dim = 1024, meta_1_depth = 1, meta_1_num_heads = 2, meta_2_depth = 1, meta_2_num_heads = 2, no dropout in meta-reasoning module, use transformer-based autoencoder in meta-reasoning module operating across candidate dimension, cls_token not learnable, apply feedback before positional encodings affixed, no consistency error on feedback signal, no skip connection around combiner, concatenate score to meta-reasoning bottleneck vector (score_rep = 1)

--- v24_clean/main.py
+++ v24_clean/main.py
@@ -13,7 +13,7 @@ from models import TransformerModelv24, DynamicWeighting, DynamicWeightingRNN
 import os
 import logging
 
-version = "v24-itr21_full"
+version = "v24-itr22_full"
 
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
@@ -78,7 +78,7 @@ def main_BERT(VERSION, RESULTS_FOLDER):
                                             meta_1_num_heads=2,
                                             meta_2_depth=1,
                                             meta_2_num_heads=2,
-                                            score_rep=8
+                                            score_rep=1
                                             ).to(device)
     if MLP_DW:
         dynamic_weights = DynamicWeighting(embed_dim=max_history_length,
