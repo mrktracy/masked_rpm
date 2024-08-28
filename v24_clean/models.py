750ab4fc5ad91a91aff661882976be87fd54ba35
mtracy03 <mtracy03@gmail.com>
2024-08-28 16:01:34 -0400

    v24-itr35_full;  20 epochs of training, use backbone perception with no MLP dropout (p=0), deconvolution decoder with no dropout (p=0), embed_dim = 512, using 1x wider symbols and TCN, ternary MLP on rows and columns with dropout (p = 0.3), transformer blocks in parallel with two sets of abstractor blocks, concatenated, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 2, abstractor one depth = 2, abstractor two depth = 2, heads = 4, backbone depth = 1, backbone heads = 4, Gamma = 0.95. Response elimination via scoring, aggregate via mean before scoring, mlp_drop = 0.5, proj_drop = 0.5, attn_drop = 0.5, weight_decay = 1e-4, mlp_ratio = 4, no L1 regularization on perception (L1 = 0), no L1 regularization on reas_meta_reas vector (L1 = 0), no data augmentation (rotations), random seed = 42, use I-RAVEN dataset, dynamic loss balancing on unsupervised terms only, BETA = 2, BATCH_SIZE = 32, restrict_qk = False, backbone transformer attn_drop = 0.3, backbone transformer proj_drop = 0.3, backbone max proj_drop = 0.5, add meta-reasoning module with feedback to perception, two-layer combiner with ReLU, transformer in meta-reasoning, feedback_dim = 1024, meta_1_depth = 1, meta_1_num_heads = 2, meta_2_depth = 1, meta_2_num_heads = 2, meta_1_proj_drop=0, meta_1_attn_drop=0, meta_1_drop_path_max=0, meta_2_proj_drop=0, meta_2_attn_drop=0, meta_2_drop_path_max=0, use transformer-based autoencoder in meta-reasoning module operating across candidate dimension, cls_token not learnable, apply feedback before positional encodings affixed, no consistency error on feedback signal, no skip connection around combiner, 15 logs per epoch, reset feedback after each evaluation, single linear layer for guesser head

--- v24_clean/models.py
+++ v24_clean/models.py
@@ -466,7 +466,7 @@ class TransformerModelv24(nn.Module): # takes in images, embeds, performs self-a
             reas_encoded = blk(x_q=reas_encoded, x_k=reas_encoded, x_v=reas_encoded)
 
         # self.feedback = self.feedback_norm.forward(reas_encoded[0, :].squeeze())
-        self.feedback = reas_encoded[0, 0, :].squeeze()
+        self.feedback = reas_encoded[0, 0, :].squeeze().clone().requires_grad_()
         # logging.info(f"self.feedback dimension: {self.feedback.size()}")
 
         # logging.info("Producing image recreation.\n")
