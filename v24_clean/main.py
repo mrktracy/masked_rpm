83467ee013bd752300e16fd1e61a49115a26111c
mtracy03 <mtracy03@gmail.com>
2024-08-29 21:45:44 -0400

    v24-itr38_full;  20 epochs of training, use backbone perception with no MLP dropout (p=0), deconvolution decoder with no dropout (p=0), embed_dim = 512, using 1x wider symbols and TCN, ternary MLP on rows and columns with dropout (p = 0.3), transformer blocks in parallel with two sets of abstractor blocks, concatenated, lr = 0.00005, Full run, all rules. CE loss between distribution over options. transformer blocks depth = 4, abstractor one depth = 4, abstractor two depth = 4, heads = 8, backbone depth = 2, backbone heads = 8, Gamma = 0.95. Response elimination via scoring, aggregate via mean before scoring, mlp_drop = 0.5, proj_drop = 0.5, attn_drop = 0.5, weight_decay = 1e-4, mlp_ratio = 4, no L1 regularization on perception (L1 = 0), no L1 regularization on reas_meta_reas vector (L1 = 0), no data augmentation (rotations), random seed = 42, use I-RAVEN dataset, BATCH_SIZE = 32, restrict_qk = False, backbone transformer attn_drop = 0.3, backbone transformer proj_drop = 0.3, backbone max proj_drop = 0.5, add meta-reasoning module with feedback to perception, two-layer combiner with ReLU, transformer in meta-reasoning, feedback_dim = 1024, meta_1_depth = 2, meta_1_num_heads = 4, meta_2_depth = 2, meta_2_num_heads = 4, meta_1_proj_drop=0, meta_1_attn_drop=0, meta_1_drop_path_max=0, meta_2_proj_drop=0, meta_2_attn_drop=0, meta_2_drop_path_max=0, use transformer-based autoencoder in meta-reasoning module operating across candidate dimension, cls_token not learnable, apply feedback before positional encodings affixed, no consistency error on feedback signal, no skip connection around combiner, 15 logs per epoch, reset feedback after each evaluation and every epoch, don't concatenate MR1 vectors before guesser head, single linear layer for guesser head, use feedback vector to set loss weightings with single linear layer, BETA = 7.5

--- v24_clean/main.py
+++ v24_clean/main.py
@@ -14,7 +14,7 @@ from models import TransformerModelv24, DynamicWeighting, DynamicWeightingRNN
 import os
 import logging
 
-version = "v24-itr38_full"
+version = "v24-itr39_full"
 
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
@@ -54,17 +54,17 @@ def main_BERT(VERSION, RESULTS_FOLDER):
 
     transformer_model = TransformerModelv24(embed_dim=512,
                                             symbol_factor=1,
-                                            trans_depth=2,
-                                            abs_1_depth=2,
-                                            abs_2_depth=2,
-                                            trans_num_heads=4,
-                                            abs_1_num_heads=4,
-                                            abs_2_num_heads=4,
+                                            trans_depth=4,
+                                            abs_1_depth=4,
+                                            abs_2_depth=4,
+                                            trans_num_heads=8,
+                                            abs_1_num_heads=8,
+                                            abs_2_num_heads=8,
                                             mlp_ratio=4,
                                             use_backbone_enc=True,
                                             decoder_num=2,  # 1 - MLP, 2 - Deconvolution, 3 - Backbone
-                                            bb_depth=1,
-                                            bb_num_heads=4,
+                                            bb_depth=2,
+                                            bb_num_heads=8,
                                             ternary_num=3, # 1 - C, 2 - Hadamard, 3 - MLP
                                             mlp_drop=0.5,
                                             proj_drop=0.5,
@@ -75,10 +75,10 @@ def main_BERT(VERSION, RESULTS_FOLDER):
                                             ternary_mlp_ratio=3,
                                             restrict_qk=False,
                                             feedback_dim=1024,
-                                            meta_1_depth=1,
-                                            meta_1_num_heads=2,
-                                            meta_2_depth=1,
-                                            meta_2_num_heads=2,
+                                            meta_1_depth=2,
+                                            meta_1_num_heads=4,
+                                            meta_2_depth=2,
+                                            meta_2_num_heads=4,
                                             score_rep=0,
                                             num_loss_terms=3,
                                             device=device
