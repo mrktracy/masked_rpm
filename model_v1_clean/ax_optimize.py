8e1b3f73a7de4f4feffbeb29bf90cf79afa72d37
mtracy03 <mtracy03@gmail.com>
2024-11-21 13:46:12 -0500

    ax_optimize

--- model_v1_clean/ax_optimize.py
+++ model_v1_clean/ax_optimize.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader
+from ax import Metric, Objective
 from ax.service.ax_client import AxClient
 from ax.service.utils.report_utils import exp_to_df
 from evaluate_masked import evaluate_model_dist as evaluation_function
@@ -80,6 +81,9 @@ def train_and_evaluate(parameterization, epochs=5):
 
 def run_optimization():
     logging.basicConfig(level=logging.INFO, filename="optimization_log.txt", filemode="w")
+
+    objective = Objective(metric=Metric(name="val_loss"), minimize=False)
+
     ax_client = AxClient()
     ax_client.create_experiment(
         name="reasoning_module_optimization",
@@ -96,10 +100,7 @@ def run_optimization():
             {"name": "bb_mlp_drop", "type": "range", "bounds": [0.0, 0.5]},
             {"name": "depth", "type": "choice", "values": [2, 4, 6, 8]},
         ],
-        objectives={
-            "name": "val_loss",
-            "minimize": True,
-        },
+        objectives=objective
     )
 
     for _ in range(20):
