285f9734f94064026f072eeb3275b95313c2042a
mtracy03 <mtracy03@gmail.com>
2025-03-12 15:45:09 -0400

    Model_v1_itr11

--- model_v1_clean/main.py
+++ model_v1_clean/main.py
@@ -75,7 +75,7 @@ def main(version, results_folder, model_class, model_params):
     FIRST_EPOCH = 0
     BATCH_SIZE = 32
     LEARNING_RATE = 0.0001
-    LOGS_PER_EPOCH = 90
+    LOGS_PER_EPOCH = 30
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 5
     ALPHA = 0.08632841418080955  # Balancing factor between task and reconstruction losses
