99bcd5583b4120315c12404e6b371fd372b9a79c
mtracy03 <mtracy03@gmail.com>
2025-03-18 17:47:30 -0400

    Model_v1_itr20

--- model_v1_clean/main.py
+++ model_v1_clean/main.py
@@ -17,7 +17,7 @@ from funs import gather_files_pgm
 from models import ReasoningModule
 
 # Versioning
-version = "Model_v1_itr19"
+version = "Model_v1_itr20"
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
 os.makedirs(results_folder, exist_ok=True)
