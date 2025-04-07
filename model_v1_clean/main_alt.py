8ed74d1f97c667677ea58e25150dfdcafe9109ff
mtracy03 <mtracy03@gmail.com>
2025-04-07 11:53:45 -0400

    Run best of hyperparams found for PGM through 50 trials with ternary only

--- model_v1_clean/main_alt.py
+++ model_v1_clean/main_alt.py
@@ -17,7 +17,7 @@ from funs import gather_files_pgm
 from models_alt import ReasoningModule
 
 # Versioning
-version = "Model_v1_itr26_pgmNeut_noDA"
+version = "Model_v1_itr27_pgmNeut_noDA"
 logfile = f"../../tr_results/{version}/runlog_{version}.txt"
 results_folder = os.path.dirname(logfile)
 os.makedirs(results_folder, exist_ok=True)
@@ -76,8 +76,8 @@ def main(version, results_folder, model_class, model_params):
     EPOCHS = 10
     FIRST_EPOCH = 0
     BATCH_SIZE = 32
-    # LEARNING_RATE = 5e-5
-    LEARNING_RATE = 1e-4
+    LEARNING_RATE = 5e-5
+    # LEARNING_RATE = 1e-4
     LOGS_PER_EPOCH = 120
     BATCHES_PER_PRINT = 20
     EPOCHS_PER_SAVE = 1
@@ -194,7 +194,7 @@ if __name__ == "__main__":
         "decoder_mlp_drop": 0.2673466611172497,
         # "symbol_factor_abs": 1,
         "symbol_factor_tern": 1,
-        "use_bb_pos_enc": True
+        "use_bb_pos_enc": False
     }
 
     main(version, results_folder, MODEL_CLASS, MODEL_PARAMS)
